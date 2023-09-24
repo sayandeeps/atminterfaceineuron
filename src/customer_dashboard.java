0a1f480bd15b60f31c3b332d621d9b36d63d1759
Sayandeep <sayandeep11@gmail.com>
2023-09-25 02:52:26 +0530

    test commit

--- src/customer_dashboard.java
+++ src/customer_dashboard.java
@@ -1,7 +1,10 @@
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class customer_dashboard {
-    
+
     public String customer;
 
     public void update() {
@@ -9,12 +12,15 @@ public class customer_dashboard {
     }
 
     void dashboard() throws IOException {
-
+        String folderPath = "./data/";
+        String filePath = folderPath + customer + ".txt";
+        String l2 = Files.readAllLines(Paths.get(filePath)).get(0);
+        // System.out.println(customer);
+        // System.out.println(l2);
         while (true) {
             Scanner sc = new Scanner(System.in);
             System.out.println("--------------------------------");
             System.out.println("PRESS 1 CHECK BALANCE");
-
 
             System.out.println("PRESS 2 FOR TRANSACTION HISTORY");
 
@@ -23,9 +29,11 @@ public class customer_dashboard {
             System.out.println("PRESS 4 FOR DEPOSIT");
 
             System.out.println("PRESS 5 TO EXIT");
-            int op=sc.nextInt();
-            if(op==1){
-                
+            int op = sc.nextInt();
+            if (op == 5) {
+                break;
+            } else if (op == 1) {
+
             }
         }
 
@@ -33,6 +41,6 @@ public class customer_dashboard {
 
     customer_dashboard(String user) {
         customer = user;
-    
+
     }
 }
