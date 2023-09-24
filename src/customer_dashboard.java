a6da0d93e7f8d69a2b76091efffa21492174a29d
Sayandeep <sayandeep11@gmail.com>
2023-09-25 04:08:52 +0530

    Deposit working

--- src/customer_dashboard.java
+++ src/customer_dashboard.java
@@ -1,22 +1,39 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class customer_dashboard {
 
     String customer;
 
-    // public void update() {
+    public void update(int bal, String filePath) {
+        try {
+            String bals = String.valueOf(bal);
+            RandomAccessFile file = new RandomAccessFile(filePath, "rw");
+            long filePointer = 0;
+            for (int i = 1; i < 2; i++) {
+                String line = file.readLine();
+                filePointer = file.getFilePointer();
+            }
+            file.seek(filePointer);
+            file.writeBytes(bals + "\n"); //
+            file.close();
 
-    // }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
     void dashboard() throws IOException {
         String folderPath = "./data/";
         String filePath = folderPath + customer + ".txt";
         String l2 = Files.readAllLines(Paths.get(filePath)).get(1);
-        System.out.println(customer);
-        System.out.println(l2);
         int cbal = Integer.parseInt(l2);
         while (true) {
             Scanner sc = new Scanner(System.in);
@@ -34,7 +51,25 @@ public class customer_dashboard {
             if (op == 5) {
                 break;
             } else if (op == 1) {
-                System.out.println(cbal);
+                System.out.println("Available Balance: " + cbal);
+            } else if (op == 4) {
+                System.out.println("--------------------------------");
+                System.out.println("Enter the amount you want to deposit");
+                int depamt = sc.nextInt();
+                cbal = cbal + depamt;
+                update(cbal, filePath);
+                System.out.println("Amount Deposited Successfully");
+                System.out.println("Present balance: " + cbal);
+                try (BufferedWriter br = new BufferedWriter(new FileWriter(filePath, true))) {
+                    Date currentDate = new Date();
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    String formattedDateTime = dateFormat.format(currentDate);
+                    br.write(depamt + " Credited at " + formattedDateTime + " Available Balance: " + cbal + "\n");
+                    br.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
             }
         }
 
